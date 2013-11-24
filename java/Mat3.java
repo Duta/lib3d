d6a010e6897ebc996ef9cf20df9d5a1e2387c4ea
Bertie Wheen <wheen.b@gmail.com>
2013-11-24 16:18:10 +0000

    Added exception handling (and minor reformatting)

--- java/Mat3.java
+++ java/Mat3.java
@@ -4,6 +4,18 @@ public class Mat3 {
     public Mat3(double m11, double m12, double m13,
                 double m21, double m22, double m23,
                 double m31, double m32, double m33) {
+        if(anyNaN(m11, m12, m13,
+                  m21, m22, m23,
+                  m31, m32, m33)) {
+            throw new IllegalArgumentException(
+                "Can't use NaN values in a matrix");
+        }
+        if(anyInfinite(m11, m12, m13,
+                       m21, m22, m23,
+                       m31, m32, m33)) {
+            throw new IllegalArgumentException(
+                "Can't use infinite values in a matrix");
+        }
         data = new double[][] {
             new double[] {m11, m12, m13},
             new double[] {m21, m22, m23},
@@ -12,6 +24,14 @@ public class Mat3 {
     }
 
     public static Mat3 xAxisRotationMatrix(double theta) {
+        if(Double.isNaN(theta)) {
+            throw new IllegalArgumentException(
+                "Can't use NaN values for theta");
+        }
+        if(Double.isInfinite(theta)) {
+            throw new IllegalArgumentException(
+                "Can't use infinite values for theta");
+        }
         double s = Math.sin(theta);
         double c = Math.cos(theta);
         return new Mat3(
@@ -21,6 +41,14 @@ public class Mat3 {
     }
 
     public static Mat3 yAxisRotationMatrix(double theta) {
+        if(Double.isNaN(theta)) {
+            throw new IllegalArgumentException(
+                "Can't use NaN values for theta");
+        }
+        if(Double.isInfinite(theta)) {
+            throw new IllegalArgumentException(
+                "Can't use infinite values for theta");
+        }
         double s = Math.sin(theta);
         double c = Math.cos(theta);
         return new Mat3(
@@ -30,6 +58,14 @@ public class Mat3 {
     }
 
     public static Mat3 zAxisRotationMatrix(double theta) {
+        if(Double.isNaN(theta)) {
+            throw new IllegalArgumentException(
+                "Can't use NaN values for theta");
+        }
+        if(Double.isInfinite(theta)) {
+            throw new IllegalArgumentException(
+                "Can't use infinite values for theta");
+        }
         double s = Math.sin(theta);
         double c = Math.cos(theta);
         return new Mat3(
@@ -38,8 +74,20 @@ public class Mat3 {
              0, 0, 1);
     }
 
-    public static Mat3 axisRotationMatrix(Vec3 v, double theta) {
-        v = v.normalize();
+    public static Mat3 axisRotationMatrix(Vec3 axis, double theta) {
+        if(axis == null) {
+            throw new IllegalArgumentException(
+                "Can't use a null axis vector");
+        }
+        if(Double.isNaN(theta)) {
+            throw new IllegalArgumentException(
+                "Can't use NaN values for theta");
+        }
+        if(Double.isInfinite(theta)) {
+            throw new IllegalArgumentException(
+                "Can't use infinite values for theta");
+        }
+        Vec3 v = axis.normalize();
         double sin = Math.sin(theta);
         double cos = Math.cos(theta);
         double negCos = 1 - cos;
@@ -58,36 +106,72 @@ public class Mat3 {
     }
 
     public static Mat3 scale(double sf) {
+        if(Double.isNaN(sf)) {
+            throw new IllegalArgumentException(
+                "Can't use NaN values for the scale factor");
+        }
+        if(Double.isInfinite(sf)) {
+            throw new IllegalArgumentException(
+                "Can't use infinite values for the scale factor");
+        }
         return Mat3.scale(sf, sf, sf);
     }
 
     public static Mat3 scale(double sfx, double sfy, double sfz) {
+        if(anyNaN(sfx, sfy, sfz)) {
+            throw new IllegalArgumentException(
+                "Can't use NaN values for the scale factors");
+        }
+        if(anyInfinite(sfx, sfy, sfz)) {
+            throw new IllegalArgumentException(
+                "Can't use infinite values for the scale factors");
+        }
         return new Mat3(
             sfx, 0, 0,
             0, sfy, 0,
             0, 0, sfz);
     }
 
-    public static Mat3 scale(Vec3 v, double sf) {
+    public static Mat3 scale(Vec3 axis, double sf) {
+        if(axis == null) {
+            throw new IllegalArgumentException(
+                "Can't use a null axis vector");
+        }
+        if(Double.isNaN(sf)) {
+            throw new IllegalArgumentException(
+                "Can't use NaN values for the scale factor");
+        }
+        if(Double.isInfinite(sf)) {
+            throw new IllegalArgumentException(
+                "Can't use infinite values for the scale factor");
+        }
         return new Mat3(
-            sf*v.getX()*v.getX() + 1,
-            sf*v.getX()*v.getY(),
-            sf*v.getX()*v.getZ(),
+            sf*axis.getX()*axis.getX() + 1,
+            sf*axis.getX()*axis.getY(),
+            sf*axis.getX()*axis.getZ(),
 
-            sf*v.getY()*v.getX(),
-            sf*v.getY()*v.getY() + 1,
-            sf*v.getY()*v.getZ(),
+            sf*axis.getY()*axis.getX(),
+            sf*axis.getY()*axis.getY() + 1,
+            sf*axis.getY()*axis.getZ(),
 
-            sf*v.getZ()*v.getX(),
-            sf*v.getZ()*v.getY(),
-            sf*v.getZ()*v.getZ() + 1);
+            sf*axis.getZ()*axis.getX(),
+            sf*axis.getZ()*axis.getY(),
+            sf*axis.getZ()*axis.getZ() + 1);
     }
 
-    public static Mat3 orthoProjection(Vec3 v) {
+    public static Mat3 orthoProjection(Vec3 axis) {
+        if(axis == null) {
+            throw new IllegalArgumentException(
+                "Can't use a null axis vector");
+        }
         return Mat3.scale(v, 0);
     }
 
-    public static Mat3 reflection(Vec3 v) {
+    public static Mat3 reflection(Vec3 axis) {
+        if(axis == null) {
+            throw new IllegalArgumentException(
+                "Can't use a null axis vector");
+        }
         return Mat3.scale(v, -1);
     }
 
@@ -97,7 +181,7 @@ public class Mat3 {
                 get(i, 1), get(i, 2), get(i, 3));
         } catch(IllegalArgumentException e) {
             throw new IllegalArgumentException(
-                "i="+i+" is not a valid row index");
+                "i="+i+" isn't a valid row index");
         }
     }
 
@@ -107,7 +191,7 @@ public class Mat3 {
                 get(1, i), get(2, i), get(3, i));
         } catch(IllegalArgumentException e) {
             throw new IllegalArgumentException(
-                "i="+i+" is not a valid column index");
+                "i="+i+" isn't a valid column index");
         }
     }
 
@@ -116,11 +200,15 @@ public class Mat3 {
             return data[i-1][j-1];
         } catch(ArrayIndexOutOfBoundsException e) {
             throw new IllegalArgumentException(
-                "i,j="+i+","+j+" is not a valid matrix index");
+                "i,j="+i+","+j+" isn't a valid matrix index");
         }
     }
 
     public Mat3 multiply(Mat3 m) {
+        if(m == null) {
+            throw new IllegalArgumentException(
+                "Can't multiply a matrix by null");
+        }
         Vec3 p = getRow(1).multiply(m);
         Vec3 q = getRow(2).multiply(m);
         Vec3 r = getRow(3).multiply(m);
@@ -128,5 +216,23 @@ public class Mat3 {
             p.getX(), p.getY(), p.getZ(),
             q.getX(), q.getY(), q.getZ(),
             r.getX(), r.getY(), r.getZ());
+    }
+
+    private static final boolean anyNaN(double... values) {
+        for(double value : values) {
+            if(Double.isNaN(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static final boolean anyInfinite(double... values) {
+        for(double value : values) {
+            if(Double.isInfinite(value)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- java/Vec3.java
+++ java/Vec3.java
@@ -1,9 +1,19 @@
 public class Vec3 {
-    private double x;
-    private double y;
-    private double z;
+    private double x, y, z;
 
     public Vec3(double x, double y, double z) {
+        if(Double.isNaN(x)
+        || Double.isNaN(y)
+        || Double.isNaN(z)) {
+            throw new IllegalArgumentException(
+                "Can't use NaN values in a vector");
+        }
+        if(Double.isInfinite(x)
+        || Double.isInfinite(y)
+        || Double.isInfinite(z)) {
+            throw new IllegalArgumentException(
+                "Can't use infinite values in a vector");
+        }
         this.x = x;
         this.y = y;
         this.z = z;
@@ -19,6 +29,14 @@ public class Vec3 {
     }
 
     public boolean equals(Vec3 v, double epsilon) {
+        if(Double.isNaN(epsilon)) {
+            throw new IllegalArgumentException(
+                "Can't use NaN values for epsilon");
+        }
+        if(Double.isInfinite(epsilon)) {
+            throw new IllegalArgumentException(
+                "Can't use infinite values for epsilon");
+        }
         if(v == null) {
             return false;
         }
@@ -40,26 +58,42 @@ public class Vec3 {
     }
 
     public Vec3 negate() {
-        return new Vec3(
-            -x, -y, -z);
+        return new Vec3(-x, -y, -z);
     }
 
     public Vec3 add(Vec3 v) {
-        return new Vec3(
-            x + v.x, y + v.y, z + v.z);
+        if(v == null) {
+            throw new NullPointerException(
+                "Can't add null to a vector");
+        }
+        return new Vec3(x + v.x, y + v.y, z + v.z);
     }
 
     public Vec3 subtract(Vec3 v) {
-        return new Vec3(
-            x - v.x, y - v.y, z - v.z);
+        if(v == null) {
+            throw new NullPointerException(
+                "Can't subtract null from a vector");
+        }
+        return new Vec3(x - v.x, y - v.y, z - v.z);
     }
 
     public Vec3 multiply(double k) {
-        return new Vec3(
-            x * k, y * k, z * k);
+        if(Double.isNaN(k)) {
+            throw new IllegalArgumentException(
+                "Can't multiply a vector by NaN values");
+        }
+        if(Double.isInfinite(k)) {
+            throw new IllegalArgumentException(
+                "Can't multiply a vector by infinite values");
+        }
+        return new Vec3(x * k, y * k, z * k);
     }
 
     public Vec3 multiply(Mat3 m) {
+        if(m == null) {
+            throw new NullPointerException(
+                "Can't multiply a vector by null");
+        }
         return new Vec3(
             dotProduct(m.getCol(1)),
             dotProduct(m.getCol(2)),
@@ -67,17 +101,34 @@ public class Vec3 {
     }
 
     public Vec3 divide(double k) {
-        return new Vec3(
-            x / k, y / k, z / k);
+        if(k == 0.0) {
+            throw new ArithmeticException(
+                "Can't divide a vector by zero");
+        }
+        if(Double.isNaN(k)) {
+            throw new IllegalArgumentException(
+                "Can't divide a vector by NaN values");
+        }
+        if(Double.isInfinite(k)) {
+            throw new IllegalArgumentException(
+                "Can't divide a vector by infinite values");
+        }
+        return new Vec3(x / k, y / k, z / k);
     }
 
     public double dotProduct(Vec3 v) {
-        return x*v.x
-             + y*v.y
-             + z*v.z;
+        if(v == null) {
+            throw new NullPointerException(
+                "Can't dot product a vector with null");
+        }
+        return x*v.x + y*v.y + z*v.z;
     }
 
     public Vec3 crossProduct(Vec3 v) {
+        if(v == null) {
+            throw new NullPointerException(
+                "Can't cross product a vector with null");
+        }
         return new Vec3(
             y*v.z - z*v.y,
             z*v.x - x*v.z,
@@ -85,6 +136,10 @@ public class Vec3 {
     }
 
     public double tripleProduct(Vec3 v1, Vec3 v2) {
+        if(v == null) {
+            throw new NullPointerException(
+                "Can't triple product a vector with null");
+        }
         return dotProduct(v1.crossProduct(v2));
     }
 
@@ -93,6 +148,10 @@ public class Vec3 {
     }
 
     public double distance(Vec3 v) {
+        if(v == null) {
+            throw new NullPointerException(
+                "Can't calculate the distance between a vector and null");
+        }
         return subtract(v).magnitude();
     }
 
